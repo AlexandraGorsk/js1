c01af27b38f381d7244906329b0c1e1f17756c59
Alexandra <alex_gorskaya1@mail.ru>
2021-10-16 23:56:49 +0300

    tabs

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
+//1
 const text =
 	'JavaScript is the programming language of the Web. The overwhelming majority of modern websites use JavaScript, and all modern web browsers — on desktops, game consoles, tablets, and smart phones —  include JavaScript interpreters, making JavaScript the most ubiquitous programming language in history. JavaScript is part of the triad of technologies that all Web  developers must learn: HTML to specify the content of web pages, CSS to specify the presentation of web pages, and JavaScript to specify the behavior of web pages.';
-let sum = 0;
-for (let i = 0; i < text.length; i++) {
-	if (text[i] === ' ') {
-		sum += 1;
-	}
+// let sum = 0;
+// for (let i = 0; i < text.length; i++) {
+// 	if (text[i] === ' ') {
+// 		sum += 1;
+// 	}
+// }
+alert(text.split(' ').length - 1);
+//2
+let array = [];
+for (let arrayNum = 0; arrayNum <= 30; arrayNum++) {
+	array.push(arrayNum);
 }
-alert(sum);
+console.log(array);
+let primes = [];
 
-let arg = [];
-for (let i = 0; i <= 30; i++) {
-	arg.push(i);
-}
-let arg2 = [];
-
-for (j = 0; j < arg.length; j++) {
-	if (arg[j] === 2) {
-		arg2.push(arg[j]);
+for (index = 0; index < array.length; index++) {
+	if (array[index] === 2) {
+		primes.push(array[index]);
 	}
-	for (g = 2; g < arg[j]; g++) {
-		if (arg[j] % g === 0) {
+	for (divider = 2; divider < array[index]; divider++) {
+		if (array[index] % divider === 0) {
 			break;
 		}
-		if (g === arg[j] - 1) {
-			arg2.push(arg[j]);
+		if (divider === array[index] - 1) {
+			primes.push(array[index]);
 		}
 	}
 }
-alert(arg2);
+alert(primes);
+//3
+let tab = function () {
+	let tabNav = document.querySelectorAll('.tabs-nav_item'),
+		tabContent = document.querySelectorAll('.tab'),
+		tabName;
+
+	tabNav.forEach((item) => {
+		item.addEventListener('click', selectTabNav);
+	});
+	function selectTabNav() {
+		tabNav.forEach((item) => {
+			item.classList.remove('is-active');
+		});
+		this.classList.add('is-active');
+		tabName = this.getAttribute('data-tab-name');
+		selectTabContent(tabName);
+	}
+	function selectTabContent(tabName) {
+		tabContent.forEach((item) => {
+			item.classList.contains(tabName)
+				? item.classList.add('is-active')
+				: item.classList.remove('is-active');
+		});
+	}
+};
+tab();
